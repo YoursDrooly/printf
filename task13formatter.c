30061ad6edf4762e5ba2a7e908b5bfa344146734
Bhavna <fabhavz@gmail.com>
2023-05-30 16:12:28 -0700

    i tried

---
+++ task13formatter.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_reverse - Prints reverse string
+ * @types: type
+ * @buffer: Buffer array
+ * @flags: flags
+ * @width: width
+ * @precision: Precision
+ * @size: Size
+ * Return: Numbers of printed chars
+ */
+
+int print_reverse(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char *str;
+	int i, count = 0;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(size);
+
+	str = va_arg(types, char *);
+
+	if (str == NULL)
+	{
+		UNUSED(precision);
+
+		str = ")Null(";
+	}
+	for (i = 0; str[i]; i++)
+		;
+
+	for (i = i - 1; i >= 0; i--)
+	{
+		char z = str[i];
+
+		write(1, &z, 1);
+		count++;
+	}
+	return (count);
+}
