f8393c39a2d13a8977935ccba1aa7dbffed9ab23
Sindi298 <sindiswamsubo71@gmail.com>
2023-05-30 14:21:51 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -10,7 +10,8 @@ void print_buffer(char buffer[], int *buff_ind);
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
+	/*int flags, width, precision, size;*/
+      	int buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -23,19 +24,19 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			print_type[buff_ind++] = format[i];
+			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
-				print_array(buffer, &buff_ind);
+				print_buffer(buffer, &buff_ind);
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
-			print_array(buffer, &buff_ind);
-			flags = get_flags(format, &i);
+			print_buffer(buffer, &buff_ind);
+			/*flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
+			size = get_size(format, &i);*/
 			++i;
 			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
@@ -45,7 +46,7 @@ int _printf(const char *format, ...)
 		}
 	}
 
-	print_array(buffer, &buff_ind);
+	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
@@ -63,4 +64,4 @@ void print_buffer(char buffer[], int *buff_ind)
 		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
-}
+}}
