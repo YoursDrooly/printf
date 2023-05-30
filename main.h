8835f71b1dcd05a70732fd2c1cb3051ea71b61e6
Sindi298 <sindiswamsubo71@gmail.com>
2023-05-29 18:07:33 -0700

    _printf.c

--- main.h
+++ main.h
@@ -5,8 +5,25 @@
 #include <stdarg.h>
 
 int print_int(va_list l, flags_t *f);
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
+
 int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 int _putchar(char c);
 int _puts(char *str);
 
+/*Functions to print char & str*/
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int widtch, int precision, int size);
+int print_pecent(va_list types, char buffer[],
+	int flags, int widtch, int precision, int size);
 #endif
